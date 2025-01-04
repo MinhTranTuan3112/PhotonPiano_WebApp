496edcd22498b29fba69f02ccfdec7d979a5176b
Trần Tuấn Minh <133552281+MinhTranTuan3112@users.noreply.github.com>
2025-01-04 15:08:41 +0700

    Update rich text editor (#3)
    * Update account layout, account profile page
    
    * Fix deployment error
    
    * Update data table config
    
    * Update rich text editor

---
+++ app/hooks/use-images-dialog.tsx
@@ -0,0 +1,169 @@
+import { Form, useFetcher } from '@remix-run/react';
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { toast } from 'sonner';
+import CustomImagesInput from '~/components/custom-images-input';
+import { Button, buttonVariants } from '~/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '~/components/ui/dialog';
+import { TEST_IMAGE_GROUP_ID } from '~/lib/utils/constants';
+import { action } from '~/routes/images';
+
+type Props = {
+    title?: string;
+    description?: string;
+    maxImages?: number;
+    defaultOpen?: boolean;
+    onConfirm: (imageUrlsResult: string[]) => void;
+    onCancel?: () => void;
+    cancelText?: string;
+    confirmText?: string;
+    cancelButtonClassname?: string;
+    confirmButtonClassname?: string;
+    requiresUpload?: boolean;
+}
+
+export type ImageFile = {
+    id: string;
+    url: string;
+    displaySize: string;
+} & File;
+
+export function useImagesDialog({
+    title = 'Thêm ảnh', description = 'Nhập ảnh từ url hoặc chọn ảnh từ thiết bị của bạn.',
+    maxImages = 1, defaultOpen = false, onConfirm, onCancel,
+    cancelText = 'Hủy', confirmText = 'Hoàn tất',
+    cancelButtonClassname, confirmButtonClassname,
+    requiresUpload = false }: Props) {
+
+    const [imageUrls, setImageUrls] = useState<string[]>([]);
+    const [imageFiles, setImageFiles] = useState<ImageFile[]>([]);
+
+    const [isOpen, setIsOpen] = useState(defaultOpen);
+
+    const fetcher = useFetcher<typeof action>();
+
+    const isUploading = fetcher.state === 'submitting';
+
+    const isDisabled = imageUrls.length === 0;
+
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const open = useCallback(() => setIsOpen(true), []);
+
+    const handleCancel = useCallback(() => {
+        setIsOpen(false)
+        onCancel?.();
+        formRef.current?.reset();
+    }, [onCancel]);
+
+    const resetForm = useCallback(() => {
+
+        setImageUrls([]);
+        setImageFiles([]);
+
+        setIsOpen(false);
+        formRef.current?.reset();
+
+        // Cleanup object URLs
+        imageFiles.forEach(file => {
+            if (file.url.startsWith('blob:')) {
+                URL.revokeObjectURL(file.url);
+            }
+        });
+
+    }, []);
+
+
+    useEffect(() => {
+
+        if (fetcher.data?.success === true) {
+            toast.success('Upload ảnh thành công!', {
+                duration: 1250
+            });
+
+            const uploadedImageUrls = fetcher.data.imageUrls;
+
+            if (uploadedImageUrls && uploadedImageUrls.length > 0) {
+                onConfirm([...imageUrls.filter((url) => !url.startsWith('blob:')), ...uploadedImageUrls]);
+            }
+
+            resetForm();
+
+
+            return;
+        }
+
+        if (fetcher.data?.success === false) {
+            toast.error(`Upload ảnh thất bại. Vui lòng thử lại sau. ${fetcher.data?.error}`);
+            return;
+        }
+
+    }, [fetcher.data]);
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault();
+
+        const currentTarget = e.currentTarget;
+
+        const formData = new FormData(currentTarget);
+
+        console.log({ imageFiles });
+
+        if (imageFiles.length > 0 && requiresUpload) {
+            fetcher.submit(formData, {
+                method: 'POST',
+                action: '/images',
+                encType: 'multipart/form-data'
+            });
+            return;
+        }
+
+        onConfirm(imageUrls);
+        
+        toast.success('Upload ảnh thành công!', {
+            duration: 1250
+        });
+
+
+        resetForm();
+
+    }
+
+    const ImagesDialog = (
+        <Dialog open={isOpen} onOpenChange={setIsOpen} >
+            <DialogContent>
+                <Form method="POST" ref={formRef} action='/images' encType="multipart/form-data"
+                    onSubmit={handleSubmit}>
+                    <input type="hidden" name='imageAction' value={'upload'} />
+                    <input type="hidden" name='groupId' value={TEST_IMAGE_GROUP_ID} />
+                    <DialogHeader className='mb-4'>
+                        <DialogTitle>{title}</DialogTitle>
+                        <DialogDescription>
+                            {description} <br /> <strong>Tối đa {maxImages} ảnh</strong>
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <CustomImagesInput imageUrls={imageUrls} setImageUrls={setImageUrls} maxImages={maxImages}
+                        isUploading={isUploading}
+                        imageFiles={imageFiles}
+                        setImageFiles={setImageFiles} />
+
+                    <DialogFooter className="flex flex-row w-full justify-between mt-4">
+                        <Button type="button" variant={'outline'} onClick={handleCancel}>{cancelText}</Button>
+                        <Button type='submit'
+                            className={
+                                confirmButtonClassname ||
+                                buttonVariants({ variant: 'theme' })
+                            }
+                            disabled={isDisabled || isUploading}
+                            isLoading={isUploading}>{confirmText}
+                        </Button>
+                    </DialogFooter>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    );
+
+    return { open, isOpen, dialog: ImagesDialog };
+}
---
+++ app/lib/services/images.ts
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+const PINATA_UPLOAD_BASE_URL = 'https://uploads.pinata.cloud/v3';
+
+export async function uploadImageFile({
+    file, name, groupId, size
+}: {
+    file: File,
+    groupId: string,
+
+} & Pick<File, "name" | "size">) {
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("name", name);
+    formData.append("group_id", groupId);
+    // formData.append("keyvalues", JSON.stringify({ size }));
+    
+
+    const response = await axios.post(`${PINATA_UPLOAD_BASE_URL}/files`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
+        },
+    });
+
+    return response;
+}
+
+export async function deleteImageFile(fileId: string) {
+
+    const response = await axios.delete(`https://api.pinata.cloud/v3/files/${fileId}`, {
+        headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
+        }
+    });
+
+    return response;
+}
+
+export async function getSignedImageUrl(fileId: string) {
+
+    const payload = JSON.stringify({
+        url: `${import.meta.env.VITE_PINATA_GATEWAY_BASE_URL}/${fileId}`,
+
+        method: "GET",
+    })
+
+    const response = await axios.post(
+        `https://api.pinata.cloud/v3/files/sign`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
+            },
+            body: payload
+        });
+
+    return await response.data;
+}
+
+export function getImageUrl(fileId: string) {
+    return `${import.meta.env.VITE_PINATA_GATEWAY_BASE_URL}/${fileId}`;
+}
+
+export async function createImageGroup({
+    name,
+    isPublic = true
+}: {
+    name: string;
+    isPublic?: boolean
+}) {
+
+    const response = await axios.post('https://api.pinata.cloud/v3/files/groups', {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
+        },
+        body: {
+            name: name,
+            is_public: isPublic
+        }
+    });
+
+    return response;
+}
