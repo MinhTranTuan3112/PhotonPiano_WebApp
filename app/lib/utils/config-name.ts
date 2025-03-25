fc352689efc22c2c99830eb8ae56f682a5626d00
kisphapha <thanhhung16082003@gmail.com>
2025-03-25 16:05:22 +0700

    fetch system config pages

--- app/lib/utils/config-name.ts
+++ app/lib/utils/config-name.ts
@@ -3,3 +3,5 @@ export const MIN_STUDENTS = "Sĩ số lớp tối thiểu"
 export const MAX_STUDENTS = "Sĩ số lớp tối đa"
 
 export const ALLOW_SKIPPING_LEVEL = "Được phép học vượt level"
+
+export const MAX_STUDENTS_IN_EXAM = "Số học viên tối đa của ca thi"
