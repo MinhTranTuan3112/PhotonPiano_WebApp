f19eb36b2b5543ba64bde04f2fcee8a67013ac71
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-04-09 11:41:26 +0700

    Improve tution page UI, UX

--- app/routes/account.tuition.tsx
+++ app/routes/account.tuition.tsx
@@ -1,29 +1,35 @@
 import type React from "react"
 import { useEffect } from "react"
-import type { LoaderFunctionArgs } from "@remix-run/node"
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node"
 import { requireAuth } from "~/lib/utils/auth"
 import { fetchPayTuition, fetchTuition } from "~/lib/services/tuition"
 import { Button } from "~/components/ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "~/components/ui/dialog"
 import { useState } from "react"
-import { useLoaderData, useNavigate, useSearchParams } from "@remix-run/react"
+import { Form, redirect, useFetcher, useLoaderData, useSearchParams } from "@remix-run/react"
 import { PaymentStatus, PaymentStatusText, type Tuition } from "~/lib/types/tuition/tuition"
-import { ArrowLeft, Download, Filter } from "lucide-react"
+import { Download, Filter } from "lucide-react"
 import { Label } from "~/components/ui/label"
 import { Checkbox } from "~/components/ui/checkbox"
 import { Badge } from "~/components/ui/badge"
 import DateRangePicker from "~/components/ui/date-range-picker"
+import { Separator } from "~/components/ui/separator"
+import { PaymentStatusBadge } from "~/components/transactions/transaction-table/columns"
+import { getErrorDetailsInfo, isRedirectError } from "~/lib/utils/error"
+import { Role } from "~/lib/types/account/account"
+import { useConfirmationDialog } from "~/hooks/use-confirmation-dialog"
+import { toast } from "sonner"
 
 export function formatDate(dateString: string | undefined) {
     if (!dateString) return "";
-    
+
     const datePart = dateString.split(' ')[0].split('T')[0];
     if (datePart && datePart.includes('-')) {
         // Convert from YYYY-MM-DD to DD-MM-YYYY
         const [year, month, day] = datePart.split('-');
         return `${day}-${month}-${year}`;
     }
-    
+
 }
 
 function calculateTotalDebt(tuition: Tuition[]): number {
@@ -34,27 +40,101 @@ function calculateTotalDebt(tuition: Tuition[]): number {
 
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-    const { idToken, role } = await requireAuth(request)
-    const url = new URL(request.url)
-    const studentClassIds = url.searchParams.getAll("student-class-ids")
-    const startTime = url.searchParams.get("start-date")
-    const endTime = url.searchParams.get("end-date")
-    const paymentStatuses = url.searchParams.getAll("payment-statuses").map(Number)
 
-    const response = await fetchTuition({
-        idToken,
-        studentClassIds,
-        startTime,
-        endTime,
-        paymentStatuses,
-    })
-    let tuition: Tuition[] = response.data
+    try {
 
-    if (!paymentStatuses.length) { // Chỉ áp dụng khi không có filter cụ thể
-        tuition = tuition.filter(fee => fee.paymentStatus !== PaymentStatus.Successed)
+        const { idToken, role } = await requireAuth(request);
+
+        if (role !== Role.Student) {
+            return redirect('/');
+        }
+
+        const url = new URL(request.url)
+        const studentClassIds = url.searchParams.getAll("student-class-ids")
+        const startTime = url.searchParams.get("start-date")
+        const endTime = url.searchParams.get("end-date")
+        const paymentStatuses = url.searchParams.getAll("payment-statuses").map(Number)
+
+        const response = await fetchTuition({
+            idToken,
+            studentClassIds,
+            startTime,
+            endTime,
+            paymentStatuses,
+        })
+        let tuition: Tuition[] = response.data
+
+        if (!paymentStatuses.length) { // Chỉ áp dụng khi không có filter cụ thể
+            tuition = tuition.filter(fee => fee.paymentStatus !== PaymentStatus.Successed)
+        }
+
+        return { tuition, idToken, role };
+
+    } catch (error) {
+
+        console.error({ error });
+
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        const { message, status } = getErrorDetailsInfo(error);
+
+        throw new Response(message, { status });
     }
-    
-    return { tuition, idToken, role }
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+    try {
+
+        const { idToken, role } = await requireAuth(request);
+
+        if (role !== Role.Student) {
+            return redirect('/');
+        }
+
+        const url = new URL(request.url);
+        const baseUrl = `${url.protocol}//${url.host}`;
+
+        const returnUrl = `${baseUrl}/account/tuition`;
+
+        const formData = await request.formData();
+
+        const tuitionId = formData.get("tuitionId") as string;
+
+        const response = await fetchPayTuition(tuitionId, returnUrl, idToken);
+
+        if (response.status === 200) {
+            const data = await response.data;
+
+            const paymentUrl = data.url as string;
+
+            return redirect(paymentUrl);
+        }
+
+        return Response.json({
+            success: false,
+            error: 'Thanh toán thất bại!',
+        }, {
+            status: 400
+        });
+
+    } catch (error) {
+        console.error({ error });
+
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        const { message, status } = getErrorDetailsInfo(error);
+
+        return Response.json({
+            success: false,
+            error: message,
+        }, {
+            status
+        });
+    }
 }
 
 export default function TuitionPage() {
@@ -63,10 +143,11 @@ export default function TuitionPage() {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [isFilterModalOpen, setIsFilterModalOpen] = useState(false)
     const [selectedFee, setSelectedFee] = useState<Tuition | null>(null)
-    const navigate = useNavigate()
+
+    const fetcher = useFetcher<typeof action>();
 
     const totalDebt = calculateTotalDebt(tuition);
-    
+
     const [filters, setFilters] = useState({
         studentClassIds: searchParams.getAll("student-class-ids"),
         dateRange: {
@@ -92,19 +173,19 @@ export default function TuitionPage() {
         setIsModalOpen(true)
     }
 
-    const handlePaymentSubmit = async (event: React.FormEvent) => {
-        event.preventDefault()
-        if (selectedFee) {
-            try {
-                const response = await fetchPayTuition(selectedFee.id, window.location.href, idToken)
-                console.log("Payment response url: ", response.data.url)
-                window.location.href = response.data.url
-            } catch (error) {
-                console.error("Payment failed", error)
-            }
-        }
-        setIsModalOpen(false)
-    }
+    // const handlePaymentSubmit = async (event: React.FormEvent) => {
+    //     event.preventDefault()
+    //     if (selectedFee) {
+    //         try {
+    //             const response = await fetchPayTuition(selectedFee.id, window.location.href, idToken)
+    //             console.log("Payment response url: ", response.data.url)
+    //             window.location.href = response.data.url
+    //         } catch (error) {
+    //             console.error("Payment failed", error)
+    //         }
+    //     }
+    //     setIsModalOpen(false)
+    // }
 
     const handlePrintInvoice = () => {
         if (!selectedFee) return;
@@ -226,6 +307,51 @@ export default function TuitionPage() {
         })
     }
 
+    const { open: handleOpenConfirmDialog, dialog: confirmDialog } = useConfirmationDialog({
+        title: "Xác nhận",
+        description: "Bạn có chắc chắn muốn thanh toán học phí không?",
+        confirmText: 'Thanh toán',
+        onConfirm: () => {
+            const tuitionId = selectedFee?.id;
+
+            if (!tuitionId) {
+                toast.error("Vui lòng chọn học phí cần thanh toán!", {
+                    position: 'top-center',
+                    duration: 1250
+                });
+                return;
+            }
+
+            const formData = new FormData();
+
+            formData.append("tuitionId", tuitionId || "");
+
+            fetcher.submit(formData, {
+                method: "POST",
+                action: "/account/tuition",
+            })
+        },
+    });
+
+    const isSubmitting = fetcher.state === 'submitting';
+
+    useEffect(() => {
+
+        if (fetcher.data?.success === false) {
+            toast.error(fetcher.data.error, {
+                position: 'top-center',
+                duration: 1250
+            });
+            return;
+        }
+
+        return () => {
+
+        }
+
+    }, [fetcher.data])
+
+
     return (
         <div className="p-4 bg-white text-black">
             <div className="max-w-5xl mx-auto">
@@ -243,7 +369,7 @@ export default function TuitionPage() {
                         <Filter className="mr-2 h-4 w-4" /> Bộ lọc
                     </Button>
                 </div>
-                <h1 className="text-3xl font-serif font-bold mb-8 text-center">Bảng học phí</h1>
+                <h1 className="text-3xl uppercase font-bold mb-8 text-center">Bảng học phí</h1>
 
                 <div className="mb-6 p-4 bg-gray-100 rounded-lg">
                     <div className="flex justify-between items-center">
@@ -265,16 +391,16 @@ export default function TuitionPage() {
                     ))
                 ) : (
                     <div className="text-center text-gray-600">
-                        Không có khoản học phí nào cần thanh toán
+                        Không có khoản học phí nào cần thanh toán.
                     </div>
                 )}
-                
+
                 {tuition.map((fee) => (
                     <div key={fee.id} className="mb-6 p-4 border border-gray-200 rounded-lg shadow-sm">
                         <div className="flex justify-between items-center mb-2">
-              <span className="text-lg font-semibold">
-                {fee.amount.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
-              </span>
+                            <span className="text-lg font-semibold">
+                                {fee.amount.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
+                            </span>
                             <Badge variant={fee.isPassed ? "destructive" : "secondary"}>
                                 Trạng thái lớp học: {fee.isPassed ? "Đã qua" : "Chưa xong"}
                             </Badge>
@@ -285,25 +411,23 @@ export default function TuitionPage() {
                         <div className="text-sm font-medium mb-2">Lớp: {fee.studentClass.className}</div>
                         <div className="text-sm text-gray-600 mb-2">Học viên: {fee.studentClass.studentFullName}</div>
                         <div className="flex justify-between items-center">
-              <span className="text-sm">
-                Trạng thái thanh toán:
-                <span
-                    className={`ml-1 font-bold ${
-                        fee.paymentStatus === PaymentStatus.Successed ? "text-green-600" : "text-gray-600"
-                    }`}
-                >
-                  {PaymentStatusText[fee.paymentStatus] || "Không xác định"}
-                </span>
-              </span>
+                            <span className="text-sm">
+                                Trạng thái thanh toán:
+                                <span
+                                    className={`ml-1 font-bold ${fee.paymentStatus === PaymentStatus.Successed ? "text-green-600" : "text-gray-600"
+                                        }`}
+                                >
+                                    {PaymentStatusText[fee.paymentStatus] || "Không xác định"}
+                                </span>
+                            </span>
                             {fee.paymentStatus !== PaymentStatus.Successed && !fee.isPassed && (
                                 <Button
                                     onClick={() => handlePayNowClick(fee)}
                                     disabled={role === 4}
-                                    className={`${
-                                        role === 4
-                                            ? "bg-gray-400 cursor-not-allowed"
-                                            : "bg-black hover:bg-gray-800"
-                                    } text-white transition-colors`}
+                                    className={`${role === 4
+                                        ? "bg-gray-400 cursor-not-allowed"
+                                        : "bg-black hover:bg-gray-800"
+                                        } text-white transition-colors`}
                                 >
                                     Trả ngay
                                 </Button>
@@ -318,21 +442,21 @@ export default function TuitionPage() {
                                     className="text-blue-600 hover:text-blue-800 flex items-center"
                                 >
                                     <Download className="mr-2 h-4 w-4" />
-                                    Download Certificate
+                                    Tải chứng chỉ
                                 </a>
                             </div>
                         )}
                     </div>
                 ))}
             </div>
-            
+
 
             <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
                 <DialogContent className="bg-white text-black">
                     <DialogHeader>
-                        <DialogTitle className="text-2xl font-serif">Xác nhận thanh toán</DialogTitle>
+                        <DialogTitle className="text-2xl ">Xác nhận thanh toán</DialogTitle>
                     </DialogHeader>
-                    <form onSubmit={handlePaymentSubmit} className="mt-4">
+                    <Form method="POST" className="mt-4">
                         <p className="mb-2">
                             Số tiền: {selectedFee?.amount.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
                         </p>
@@ -348,37 +472,42 @@ export default function TuitionPage() {
                         >
                             In giấy báo học phí
                         </Button>
-                        
+
                         <Button
-                            type="submit"
+                            type="button"
                             className="w-full bg-black text-white hover:bg-gray-800 transition-colors py-2 px-4 rounded"
+                            onClick={handleOpenConfirmDialog}
+                            isLoading={isSubmitting}
+                            disabled={isSubmitting}
                         >
-                            Tiến hành thanh toán
+                            {isSubmitting ? "Đang xử lý..." : "Xác nhận thanh toán"}
                         </Button>
-                    </form>
+                    </Form>
                 </DialogContent>
             </Dialog>
 
             <Dialog open={isFilterModalOpen} onOpenChange={setIsFilterModalOpen}>
                 <DialogContent className="bg-white text-black">
                     <DialogHeader>
-                        <DialogTitle className="text-2xl font-serif">Bộ lọc</DialogTitle>
+                        <DialogTitle className="text-2xl flex flex-row gap-1 items-center"><Filter /> Bộ lọc</DialogTitle>
+                        <Separator className="w-full" />
                     </DialogHeader>
-                    <form onSubmit={handleFilterSubmit} className="mt-4 space-y-4">
-                        <div>
-                            <Label>Ngày từ - đến</Label>
+                    <form onSubmit={handleFilterSubmit} className="mt-4 space-y-4 flex flex-col gap-2">
+                        <div className="flex flex-col gap-2">
+                            <Label className="font-bold my-3 text-base">Ngày từ - đến</Label>
                             <DateRangePicker
                                 value={filters.dateRange}
                                 onChange={(dateRange) => setFilters({ ...filters, dateRange: dateRange || {} })}
                                 placeholder="Chọn khoảng thời gian"
                             />
                         </div>
-                        <div>
-                            <Label>Trạng thái</Label>
-                            <div className="flex flex-wrap gap-2">
+
+                        <div className="flex flex-col gap-2">
+                            <Label className="font-bold my-3 text-base">Trạng thái</Label>
+                            <div className="flex flex-col gap-3">
                                 {Object.entries(PaymentStatus)
                                     .filter(([key]) => isNaN(Number(key)))
-                                    .map(([key, value]) => (
+                                    .map(([key, value], index) => (
                                         <div key={key} className="flex items-center">
                                             <Checkbox
                                                 id={`status-${value}`}
@@ -386,21 +515,23 @@ export default function TuitionPage() {
                                                 onCheckedChange={(checked) => handlePaymentStatusChange(Number(value), checked === true)}
                                             />
                                             <Label htmlFor={`status-${value}`} className="ml-2">
-                                                {key}
+                                                <PaymentStatusBadge status={index} />
                                             </Label>
                                         </div>
                                     ))}
                             </div>
+
                         </div>
                         <Button
                             type="submit"
                             className="w-full bg-black text-white hover:bg-gray-800 transition-colors py-2 px-4 rounded"
                         >
-                            Xác nhận
+                            Tìm kiếm
                         </Button>
                     </form>
                 </DialogContent>
             </Dialog>
+            {confirmDialog}
         </div>
     )
 }
