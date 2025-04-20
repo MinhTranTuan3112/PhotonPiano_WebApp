7577e5d43e1bb791bf571a9bc86ae895da21855b
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-04-20 14:16:24 +0700

    Fix admission flow: add more validation rules for create and update entrance test

--- app/lib/utils/form.ts
+++ app/lib/utils/form.ts
@@ -19,7 +19,41 @@ export const formEntryToDateOnly = (formEntry?: FormDataEntryValue | null, defau
     return date.toISOString().split('T')[0]
 }
 
-export const formEntryToNumber = (formEntry?: FormDataEntryValue | null, defaultValue?: number ) => {
+export const formEntryToNumber = (formEntry?: FormDataEntryValue | null, defaultValue?: number) => {
     var value = formEntry?.toString().replace(/"/g, "").trim()
-    return value ? Number.parseInt(value) : (defaultValue  ?? undefined)
+    return value ? Number.parseInt(value) : (defaultValue ?? undefined)
+}
+
+export function objectToFormData(
+    obj: Record<string, any>,
+    form: FormData = new FormData(),
+    namespace?: string
+): FormData {
+    for (const key in obj) {
+        if (!obj.hasOwnProperty(key) || obj[key] === undefined || obj[key] === null) continue;
+
+        const formKey = namespace ? `${namespace}[${key}]` : key;
+        const value = obj[key];
+
+        if (value instanceof Date) {
+            form.append(formKey, value.toISOString());
+        } else if (value instanceof File || value instanceof Blob) {
+            form.append(formKey, value);
+        } else if (Array.isArray(value)) {
+            value.forEach((item, index) => {
+                const arrayKey = `${formKey}[${index}]`;
+                if (typeof item === 'object') {
+                    objectToFormData(item, form, arrayKey);
+                } else {
+                    form.append(arrayKey, item);
+                }
+            });
+        } else if (typeof value === 'object') {
+            objectToFormData(value, form, formKey);
+        } else {
+            form.append(formKey, value);
+        }
+    }
+
+    return form;
 }
