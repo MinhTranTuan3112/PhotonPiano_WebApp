c8e5e0d70e57f2c28420efa0d822fa504beeac73
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-04-20 19:39:09 +0700

    CRUD day offs

--- app/lib/types/day-off/day-off.ts
+++ app/lib/types/day-off/day-off.ts
@@ -1,7 +1,26 @@
+import { z } from "zod";
+
 export type DayOff = {
-    name : string,
-    startTime : string,
-    endTime : string,
-    createdAt : string,
-    createdById : string
+    id: string;
+    name: string,
+    startTime: string,
+    endTime: string,
+    createdAt: string,
+    createdById: string
 }
+
+export type CreateDayOffRequest = Pick<DayOff, "name" | "startTime" | "endTime">;
+
+export type UpdateDayOffRequest = Partial<CreateDayOffRequest> & {
+    id: string;
+}
+
+export const dayOffSchema = z.object({
+    dayOffAction: z.string(),
+    id: z.string().optional(),
+    name: z.string(),
+    startTime: z.coerce.date(),
+    endTime: z.coerce.date()
+});
+
+export type DayOffFormData = z.infer<typeof dayOffSchema>;
--- app/routes/admin.day-offs.tsx
+++ app/routes/admin.day-offs.tsx
@@ -1,49 +1,23 @@
-import { Await, Form, useFetcher, useLoaderData, useSearchParams } from '@remix-run/react'
+import { Await, useFetcher, useLoaderData } from '@remix-run/react'
 import { Suspense, useEffect, useState } from 'react'
 import { Skeleton } from '~/components/ui/skeleton'
-import { ActionFunctionArgs, data, LoaderFunctionArgs, redirect } from '@remix-run/node'
+import { ActionFunctionArgs, LoaderFunctionArgs, redirect } from '@remix-run/node'
 import { Role } from '~/lib/types/account/account'
 import { getErrorDetailsInfo, isRedirectError } from '~/lib/utils/error'
 import { requireAuth } from '~/lib/utils/auth'
-import { fetchAllMinimalCriterias, fetchCreateCriteria, fetchCriterias, fetchDeleteCriteria, fetchUpdateCriteria } from '~/lib/services/criteria'
-import { Criteria, CriteriaFor } from '~/lib/types/criteria/criteria'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-import { formEntryToNumber, formEntryToString } from '~/lib/utils/form'
 import { Button } from '~/components/ui/button'
-import { CheckIcon, Delete, Edit2Icon, PlusCircle, X, XIcon } from 'lucide-react'
-import { Input } from '~/components/ui/input'
-import { useRemixForm } from 'remix-hook-form'
-import { ActionResult } from '~/lib/types/action-result'
-import { useConfirmationDialog } from '~/hooks/use-confirmation-dialog'
-import useLoadingDialog from '~/hooks/use-loading-dialog'
-import { fetchRooms } from '~/lib/services/rooms'
-import { Room } from '~/lib/types/room/room'
 import GenericDataTable from '~/components/ui/generic-data-table'
-import { roomColumns } from '~/components/admin/table/room-column'
 import { PaginationMetaData } from '~/lib/types/pagination-meta-data'
-import { fetchDayOffs } from '~/lib/services/day-off'
-import { DayOff } from '~/lib/types/day-off/day-off'
-import { dayOffColumns } from '~/components/admin/table/day-off-column'
+import { fetchCreateDayOff, fetchDayOffs, fetchDeleteDayOff, fetchUpdateDayOff } from '~/lib/services/day-off'
+import { DayOff, dayOffSchema } from '~/lib/types/day-off/day-off'
+import { dayOffColumns, DayOffDialog } from '~/components/admin/table/day-off-column'
+import { PlusCircle } from 'lucide-react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { getValidatedFormData } from 'remix-hook-form'
+import { toast } from 'sonner'
+
 type Props = {}
-
-const addCriteriaSchema = z
-    .object({
-        description: z.string().optional(), // Optional URL for existing avatar
-        name: z
-            .string({ message: "Tên không được để trống." }),
-        weight: z.coerce.number({ message: "Trọng số không được để trống" })
-            .min(1, { message: "Tối thiểu 1." })
-            .max(100, { message: "Tối đa 100." }),
-        action: z.string(),
-        idToken: z.string(),
-        criteriaFor: z.coerce.number(),
-    });
-
-type AddCriteriaSchema = z.infer<typeof addCriteriaSchema>;
-
-const resolver = zodResolver(addCriteriaSchema);
 
 export async function loader({ request }: LoaderFunctionArgs) {
 
@@ -55,7 +29,17 @@ export async function loader({ request }: LoaderFunctionArgs) {
             return redirect('/');
         }
 
-        const promise = fetchDayOffs({ idToken, pageSize: 100 }).then((response) => {
+        const { searchParams } = new URL(request.url);
+
+        const query = {
+            page: Number.parseInt(searchParams.get('page') || '1'),
+            pageSize: Number.parseInt(searchParams.get('size') || '100'),
+            sortColumn: searchParams.get('column') || 'Id',
+            orderByDesc: searchParams.get('desc') === 'true' ? true : false,
+            idToken
+        };
+
+        const promise = fetchDayOffs({ ...query }).then((response) => {
             const dayOffs = response.data as DayOff[];
 
             const headers = response.headers;
@@ -65,12 +49,13 @@ export async function loader({ request }: LoaderFunctionArgs) {
                 pageSize: parseInt(headers['x-page-size'] || '10'),
                 totalPages: parseInt(headers['x-total-pages'] || '1'),
                 totalCount: parseInt(headers['x-total-count'] || '0'),
-
             };
-            return { dayOffs , metadata};
+
+            return { dayOffs, metadata };
         });
 
         return {
+            query,
             promise,
             role,
             idToken
@@ -90,45 +75,162 @@ export async function loader({ request }: LoaderFunctionArgs) {
     }
 }
 
+const schema = dayOffSchema.extend({
+    dayOffAction: z.string()
+});
+
+type ServerFormData = z.infer<typeof schema>;
 
 export async function action({ request }: ActionFunctionArgs) {
 
     try {
 
-    } catch (err) {
-        var error = getErrorDetailsInfo(err)
-        return {
-            success: false,
-            error: error.message,
-            status: error.status
+        const { idToken, role } = await requireAuth(request);
+
+        if (role !== Role.Administrator) {
+            return redirect('/');
         }
+
+        const { searchParams } = new URL(request.url);
+
+        const dayOffAction = searchParams.get('action') as string;
+
+        if (dayOffAction === 'delete') {
+            await fetchDeleteDayOff({
+                id: searchParams.get('id') || '',
+                idToken
+            });
+
+            return Response.json({
+                success: true,
+                dayOffAction: 'delete'
+            }, {
+                status: 200
+            });
+        }
+
+        const { errors, data, receivedValues: defaultValues } =
+            await getValidatedFormData<ServerFormData>(request, zodResolver(schema));
+
+        if (errors) {
+            return { success: false, errors, defaultValues };
+        }
+
+        switch (data.dayOffAction.toLowerCase()) {
+            case 'update':
+
+                await fetchUpdateDayOff({
+                    ...data,
+                    id: data.id || '',
+                    startTime: data.startTime ? data.startTime.toISOString() : undefined,
+                    endTime: data.endTime ? data.endTime.toISOString() : undefined,
+                    idToken
+                })
+
+                break;
+
+            case 'create':
+
+                await fetchCreateDayOff({
+                    ...data,
+                    startTime: data.startTime.toISOString(),
+                    endTime: data.endTime.toISOString(),
+                    idToken
+                })
+
+                break;
+
+            case 'delete':
+
+                await fetchDeleteDayOff({
+                    id: data.id || '',
+                    idToken
+                })
+
+                break;
+
+            default:
+                break;
+        }
+
+        return Response.json({
+            success: true,
+            dayOffAction: data.dayOffAction,
+        }, {
+            status: 200
+        });
+
+    } catch (error) {
+        console.error({ error });
+
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        const { message, status } = getErrorDetailsInfo(error);
+
+        return Response.json({
+            success: false,
+            error: message,
+            status
+        }, {
+            status
+        })
+
     }
 
 }
 
 export default function AdminDayOff({ }: Props) {
 
-    const { promise, idToken } = useLoaderData<typeof loader>();
+    const { promise, query } = useLoaderData<typeof loader>();
+
+    const [isOpenDialog, setIsOpenDialog] = useState(false);
+
+    const fetcher = useFetcher<typeof action>();
+
+    useEffect(() => {
+
+        if (fetcher.data?.success === true) {
+            toast.success('Tạo kỳ nghỉ thành công!');
+            setIsOpenDialog(false);
+            return;
+        }
+
+
+        if (fetcher.data?.success === false) {
+            toast.warning(fetcher.data.error, {
+                duration: 5000
+            });
+            return;
+        }
+
+        return () => {
+
+        }
+        
+    }, [fetcher.data]);
 
     return (
-        <article className='px-10'>
-            <h1 className="text-xl font-extrabold">Quản lý các kỳ nghỉ</h1>
-            <p className='text-muted-foreground'>Quản lý các kỳ nghỉ, phục vụ cho việc sắp xếp lớp</p>
-            <div className='my-2  flex justify-end w-full'>
-                <Button Icon={PlusCircle} iconPlacement='left'>Thêm mới</Button>
-            </div>
-            <Suspense fallback={<LoadingSkeleton />}>
-                <Await resolve={promise}>
-                    {(data) => (
-                        <GenericDataTable columns={dayOffColumns} resolvedData={data.dayOffs} metadata={data.metadata} />
-                    )}
-                </Await>
-            </Suspense >
-            {/* {loadingAddDialog}
-            {confirmAddDialog}
-            {confirmDeleteDialog}
-            {confirmEditModal} */}
-        </article >
+        <>
+            <article className='px-10'>
+                <h1 className="text-xl font-extrabold">Quản lý các kỳ nghỉ</h1>
+                <p className='text-muted-foreground text-sm'>Quản lý các kỳ nghỉ, phục vụ cho việc sắp xếp lớp</p>
+                <div className='my-2 flex justify-end w-full'>
+                    <Button Icon={PlusCircle} iconPlacement='left' type='button'
+                        onClick={() => setIsOpenDialog(true)}>Thêm mới</Button>
+                </div>
+                <Suspense fallback={<LoadingSkeleton />} key={JSON.stringify(query)}>
+                    <Await resolve={promise}>
+                        {(data) => (
+                            <GenericDataTable columns={dayOffColumns} resolvedData={data.dayOffs} metadata={data.metadata} />
+                        )}
+                    </Await>
+                </Suspense>
+            </article>
+            <DayOffDialog fetcher={fetcher} isOpen={isOpenDialog}
+                setIsOpen={setIsOpenDialog} isEdit={false} />
+        </>
     )
 }
 
