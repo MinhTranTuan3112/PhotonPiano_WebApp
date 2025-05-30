887364f8106d962c5c7d44a7a7759a158b1b8fc3
kisphapha <thanhhung16082003@gmail.com>
2025-05-30 12:08:27 +0700

    add tuition information in class detail

--- app/lib/types/class/class-detail.ts
+++ app/lib/types/class/class-detail.ts
@@ -1,13 +1,13 @@
 
 import { Slot } from "../Scheduler/slot";
 import { Class } from "./class";
-import { StudentClassWithScore, StudentClassWithStudent } from "./student-class";
+import { StudentClassDetails, StudentClassWithScore } from "./student-class";
 
 export type ClassDetail = {
     pricePerSlots : number,
     slotsPerWeek : number,
     slots : Slot[],
-    studentClasses : StudentClassWithStudent[]
+    studentClasses : StudentClassDetails[]
 } & Class
 
 export type ClassScoreDetail = {
